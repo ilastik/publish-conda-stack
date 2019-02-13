578be4c8dac11bbf343455ce69685e11738feb01
k-dominik <k-dominik@users.noreply.github.com>
2019-02-13 14:00:27 +0100

    test auto deploy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="publish-conda-stack",
-    version="0.2.3dev5",
+    version="0.2.3dev6",
     author="Stuart Berg, Carsten Haubold",
     author_email="team@ilastik.org",
     license="MIT",
