3e27e44cc396c42e145ab33e35ea816665400594
Stuart Berg <bergs@janelia.hhmi.org>
2019-05-13 10:37:44 -0400

    Bump version: 0.2.3dev20 → 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="publish-conda-stack",
-    version="0.2.3dev20",
+    version="0.2.3",
     author="Stuart Berg, Carsten Haubold",
     author_email="team@ilastik.org",
     license="MIT",
