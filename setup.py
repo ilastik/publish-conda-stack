52eb5a4d1b3a5b717f94077b4e28d6e1026f6bdb
k-dominik <k-dominik@users.noreply.github.com>
2021-11-12 16:51:32 +0100

    Revert "added activate.sh script in hopes this will disable activation on win"
    This reverts commit 4846971efff0a860bf4294e2b91761c6937900af.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="publish-conda-stack",
-    version="0.3.0dev6",
+    version="0.3.0dev5",
     author="Stuart Berg, Carsten Haubold",
     author_email="team@ilastik.org",
     license="MIT",
