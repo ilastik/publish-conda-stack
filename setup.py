5a10ddb62621ed39b10570a4daad868a80c6ccf6
k-dominik <k-dominik@users.noreply.github.com>
2024-02-06 17:26:35 +0100

    release 0.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="publish-conda-stack",
-    version="0.4.2",
+    version="0.4.3",
     author="Stuart Berg, Carsten Haubold",
     author_email="team@ilastik.org",
     license="MIT",
