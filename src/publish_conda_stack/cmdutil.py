d633a5433621a157d07e60dab132e495c403e19f
k-dominik <k-dominik@users.noreply.github.com>
2024-02-06 14:41:13 +0100

    mamba seems to not support render anymore -> conda
    made sure render command is run via conda, no matter what backend is
    selected.

--- src/publish_conda_stack/cmdutil.py
+++ src/publish_conda_stack/cmdutil.py
@@ -13,6 +13,17 @@ class CondaCommand(IntEnum):
 
 
 def conda_cmd_base(command: CondaCommand, shared_config: dict) -> List[str]:
+    variant_config = shared_config.get("master-conda-build-config", None)
+    variant_args = ["-m", variant_config] if variant_config else []
+
+    # render is not supported in mamba, here we must use conda
+    if command == CondaCommand.RENDER:
+        args = ["conda"]
+        args.extend(["render", "--output"])
+        args.extend(shared_config["conda-source-channel-list"])
+        args.extend(variant_args)
+        return args
+
     backend = shared_config.get("backend", DEFAULT_BACKEND)
     if backend not in ["conda", "mamba"]:
         raise ValueError(
@@ -20,14 +31,8 @@ def conda_cmd_base(command: CondaCommand, shared_config: dict) -> List[str]:
         )
 
     args = [backend]
-    variant_config = shared_config.get("master-conda-build-config", None)
-    variant_args = ["-m", variant_config] if variant_config else []
-    if command == CondaCommand.RENDER:
-        args.extend(["render", "--output"])
-        args.extend(shared_config["conda-source-channel-list"])
-        args.extend(variant_args)
-        return args
-    elif command == CondaCommand.SEARCH:
+
+    if command == CondaCommand.SEARCH:
         labels = shared_config.get("labels", [])
         args.extend(
             [
@@ -41,7 +46,8 @@ def conda_cmd_base(command: CondaCommand, shared_config: dict) -> List[str]:
         )
         args.extend(labels_to_search_args(shared_config["upload-channel"], labels))
         return args
-    elif command == CondaCommand.BUILD:
+
+    if command == CondaCommand.BUILD:
         if backend == "mamba":
             args = ["conda", "mambabuild"]
         else:
@@ -49,5 +55,5 @@ def conda_cmd_base(command: CondaCommand, shared_config: dict) -> List[str]:
         args.extend(shared_config["conda-source-channel-list"])
         args.extend(variant_args)
         return args
-    else:
-        raise ValueError(f"unknown command supplied. Got {command}")
+
+    raise ValueError(f"unknown command supplied. Got {command}")
